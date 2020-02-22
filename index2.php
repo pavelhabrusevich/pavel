af7fbefdac2bd00523719802c01d8b155f6365f1
pavel.habrusevich <pavel.habrusevich@gmail.com>
2020-02-22 20:56:51 +0300

    bullShit: final result

--- index2.php
+++ index2.php
@@ -8,17 +8,15 @@ getTaxonomy() - возвращает фрукт это или овощ
 которая будет считать объём плода (будем считать, что они все параллелепипеды)
 Также реализовать константы в классах для имени этого овобща или фрукта на Французском языке.
 Использовать всю мощь public, private и protected свойств
-
 вопросы:
 - можно ли константу запихнуть в конструктор?
-- мне не нравится метод getTaxonomy(), поттому что нет класса ОВОЩИ аналогичного fruit.
-- сначала сделал методы в родительском класссе protected, а после public, потому что не было нужды переопреелять методы */
+ */
 class Plant{
     private $length, $width, $height, $plantNameName;
     public $averageWeight = [];
-    public function __construct($length, $width, $height, $plantName)
+    protected function __construct($length, $width, $height, $plantName)
     {
-        return "Объём самой большой " . $plantName . " " . $size = $length * $width * $height . " единиц.<br/>";
+        echo "Объём самой большой " . $plantName . " " . $size = $length * $width * $height . " единиц.<br/>";
     }
     public function getWeight($plantName)
     {
@@ -27,38 +25,54 @@ class Plant{
             return "Средний вес $plantName " . $allPlants = array_sum($allPlants)/count($allPlants) . ".<br/>";
         } else echo "Насыпь $plantName";
     }
+    protected function getTaxonomy(){
+        return "Овощ" . "<br/>";
+    }
 }
 
 class Potato extends Plant {
     private const POTATO = "Pommes de terre\n";
-    public function getTaxonomy(){
-        return "Овощ" . "<br/>";
+    public function __construct($length, $width, $height, $plantName)
+    {
+        parent::__construct($length, $width, $height, $plantName);
+    }
+    public function getTaxonomy()
+    {
+        return parent::getTaxonomy();
     }
     public function potatoFrenchName()
     {
-        if (defined("self::POTATO")) return self::POTATO . "<br/>";
+        if (defined("self::POTATO")){
+            return self::POTATO . "<br/>";}
+        else return "Переведи на французский" . "<br/>";
     }
 }
 
 class Fruit extends Plant{
-    public function getTaxonomy(){
+    public function getTaxonomy()
+    {
         return "Фрукт" . "<br/>";
     }
 }
 
 class Banana extends Fruit{
     private const BANANA = "La banane\n";
+    public function __construct($length, $width, $height, $plantName)
+    {
+        parent::__construct($length, $width, $height, $plantName);
+    }
     public function bananoFrenchName()
     {
-        if (defined("self::BANANA")) return self::BANANA . "<br/>";
-    }
+        if (defined("self::BANANA")){
+            return self::BANANA . "<br/>";}
+        else return "Переведи на французский" . "<br/>";
+        }
 }
-
+// Примеры использования классов:
 // Объект "картоха"
-$obj = new Potato(2,3,4, "картохи");
+$obj = new Potato(3,3,4, "картохи");
 echo $obj->getTaxonomy();
 echo $obj->potatoFrenchName();
-echo $obj->__construct(2,4,3,"картохи");
 $obj->averageWeight = [2,2,9];
 echo $obj->getWeight("картохи");
 
@@ -66,6 +80,5 @@ echo $obj->getWeight("картохи");
 $obj2 = new Banana(8,3,4, "бананчиков");
 echo $obj2->getTaxonomy();
 echo $obj2->bananoFrenchName();
-echo $obj2->__construct(8,3,4, "бананчиков");
 $obj2->averageWeight = [2,4,3];
 echo $obj2->getWeight("бананчиков");
--- index3.php
+++ index3.php
@@ -1,24 +1,21 @@
 <?php
-/*Реализовать четыре класса : Fruit, Banana, Potato, Plant
-Plant родитель для Potato и Fruit, Fruit родитель для Banana
-Реализовать методы
-getWeight() - возвращает средний вес
-getTaxonomy() - возвращает фрукт это или овощ
-Реализовать конструктор в который передаются высота, ширина и длина и требуется реализовать функцию,
-которая будет считать объём плода (будем считать, что они все параллелепипеды)
-Также реализовать константы в классах для имени этого овобща или фрукта на Французском языке.
-Использовать всю мощь public, private и protected свойств
-
-вопросы:
-- можно ли константу запихнуть в конструктор?
-- мне очень не нравится метод getTaxonomy(), поттому что нет класса ОВОЩИ аналогичного fruit.
-были мысли запихнуть этот метод в класс Plant а далее в проверять: если  */
-class Plant{
+/*
+Реализовать класс Vegetable и внедрить его в текущую структуру. Подумать что может быть abstract, final.  */
+abstract class Plant{
     private $length, $width, $height, $plantNameName;
     public $averageWeight = [];
+    protected function __construct($length, $width, $height)
+    {
+    }
+    protected function getWeight($plantName)
+    {
+    }
+}
+
+class Vegetable extends Plant{
     public function __construct($length, $width, $height, $plantName)
     {
-        return "Объём самой большой " . $plantName . " " . $size = $length * $width * $height . " единиц.<br/>";
+        echo "Объём самой большой " . $plantName . " " . $size = $length * $width * $height . " единиц.<br/>";
     }
     public function getWeight($plantName)
     {
@@ -27,45 +24,61 @@ class Plant{
             return "Средний вес $plantName " . $allPlants = array_sum($allPlants)/count($allPlants) . ".<br/>";
         } else echo "Насыпь $plantName";
     }
-}
-
-class Potato extends Plant {
-    private const POTATO = "Pommes de terre\n";
-    public function getTaxonomy(){
+    public function getTaxonomy()
+    {
         return "Овощ" . "<br/>";
     }
+}
+
+final class Potato extends Vegetable {
+    private const POTATO = "Pommes de terre\n";
     public function potatoFrenchName()
     {
-        if (defined("self::POTATO")) return self::POTATO . "<br/>";
+        if (defined("self::POTATO")){
+            return self::POTATO . "<br/>";}
+        else return "Переведи на французский" . "<br/>";
     }
 }
 
 class Fruit extends Plant{
-    public function getTaxonomy(){
+    public function __construct($length, $width, $height, $plantName)
+    {
+        echo "Объём самой большой " . $plantName . " " . $size = $length * $width * $height . " единиц.<br/>";
+    }
+    public function getWeight($plantName)
+    {
+        $allPlants = $this->averageWeight;
+        if (!empty($allPlants)){
+            return "Средний вес $plantName " . $allPlants = array_sum($allPlants)/count($allPlants) . ".<br/>";
+        } else echo "Нарви $plantName";
+    }
+    public function getTaxonomy()
+    {
         return "Фрукт" . "<br/>";
     }
 }
 
-class Banana extends Fruit{
+final class Banana extends Fruit{
     private const BANANA = "La banane\n";
     public function bananoFrenchName()
     {
-        if (defined("self::BANANA")) return self::BANANA . "<br/>";
+        if (defined("self::BANANA")){
+            return self::BANANA . "<br/>";}
+        else return "Переведи на французский" . "<br/>";
     }
 }
 
-// Объект "картоха"
-$obj = new Potato(2,3,4, "картохи");
+// Примеры использования классов:
+// Объект "Картоха"
+$obj = new Potato(3,3,4, "картохи");
 echo $obj->getTaxonomy();
 echo $obj->potatoFrenchName();
-echo $obj->__construct(2,4,3,"картохи");
-$obj->averageWeight = [2,2,9];
+$obj->averageWeight = [3,2,9];
 echo $obj->getWeight("картохи");
 
 // Объект "Банан"
 $obj2 = new Banana(8,3,4, "бананчиков");
 echo $obj2->getTaxonomy();
 echo $obj2->bananoFrenchName();
-echo $obj2->__construct(8,3,4, "бананчиков");
 $obj2->averageWeight = [2,4,3];
 echo $obj2->getWeight("бананчиков");
